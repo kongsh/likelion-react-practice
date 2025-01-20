90e7552a4630a069c671d31c1772de8bf1ee0c6c
kongsh <ksh080838@gmail.com>
2025-01-20 10:27:14 +0900

    [구성] vite-react 초기 구성 불러오기

---
+++ src/app/app.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from 'react';
+import './app.css';
+
+function App(props: { children?: ReactElement }) {
+  return <div className="app">{props?.children}</div>;
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'node:url';
+
+const viteConfig = defineConfig({
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+    }),
+  ],
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
+  preview: {
+    host: 'localhost',
+    port: 8080,
+  },
+  css: {
+    devSourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
+
+export default viteConfig;
