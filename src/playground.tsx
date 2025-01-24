ff5f9303baee62d41df6aff6f131bddff39557b7
kongsh <ksh080838@gmail.com>
2025-01-24 16:12:09 +0900

    [실습] 로그인 기능 구현

--- src/playground.tsx
+++ src/playground.tsx
@@ -1,10 +1,12 @@
 import SignUpForm from './playgrounds/03-signup-form';
+import SignInForm from './playgrounds/04-signin-form';
 
 function Playground() {
   return (
     <div className="playground">
       <h1>플레이그라운드</h1>
       <SignUpForm></SignUpForm>
+      <SignInForm></SignInForm>
     </div>
   );
 }
