e21d44b98406d35ad225dd3d2d77b16db1c4d3ba
kongsh <ksh080838@gmail.com>
2025-02-04 09:17:10 +0900

    [실습] 네비게이션 리스트 데이터 렌더링
    - 리스트 렌더링
    - 네비게이션 리스트 데이터 작성

--- src/homework/components/nav/index.tsx
+++ src/homework/components/nav/index.tsx
@@ -1,54 +1,43 @@
+import './style.css';
+import NavLink from './nav-link';
 import { useState } from 'react';
 import { getUIView, type UIView } from '@/homework/lib/ui-view';
-import './style.css';
-import clsx from 'clsx/lite';
-
-// console.log(clsx('a', 'b', true && 'c'));
-// console.log(clsx('a', 'b', false && 'c'));
 
 function Nav() {
   const [uiView] = useState<UIView>(getUIView);
-
-  switch (uiView) {
-    case 'signin':
-      break;
-    case 'signup':
-      break;
-    case 'state-management':
-      break;
-  }
 
   const isSignInView = uiView.includes('signin');
   const isSignUpView = uiView.includes('signup');
   const isStateManagementView = uiView.includes('state-management');
 
+  const navList = [
+    { id: 'item-1', content: '로그인', href: 'signin', isActive: isSignInView },
+    {
+      id: 'item-2',
+      content: '회원가입',
+      href: 'signup',
+      isActive: isSignUpView,
+    },
+    {
+      id: 'item-3',
+      content: '상태 관리 전략',
+      href: 'state-management',
+      isActive: isStateManagementView,
+    },
+  ];
+
   return (
     <nav className="nav">
       <h2 className="sr-only">페이지 탐색</h2>
-      <a
-        href="/?view=signin"
-        // className={isSignInView ? 'active' : undefined}
-        className={clsx(isSignInView && 'active')}
-        aria-current={isSignInView ? 'page' : undefined}
-      >
-        로그인
-      </a>
-      <a
-        href="/?view=signup"
-        // className={!isSignInView ? 'active' : undefined}
-        className={clsx(isSignUpView && 'active')}
-        aria-current={isSignUpView ? 'page' : undefined}
-      >
-        회원가입
-      </a>
-      <a
-        href="/?view=state-management"
-        // className={!isSignInView ? 'active' : undefined}
-        className={clsx(isStateManagementView && 'active')}
-        aria-current={isStateManagementView ? 'page' : undefined}
-      >
-        상태 관리 전략
-      </a>
+      {navList.map((navItem) => (
+        <NavLink
+          key={navItem.id}
+          href={navItem.href}
+          isActive={navItem.isActive}
+        >
+          {navItem.content}
+        </NavLink>
+      ))}
     </nav>
   );
 }
--- src/homework/pages/state-management/index.tsx
+++ src/homework/pages/state-management/index.tsx
@@ -1,5 +1,41 @@
+import { tm } from '@/utils/tw-merge';
+import { useState } from 'react';
+
 function StateManagement() {
-  return <>상태관리</>;
+  return (
+    <section>
+      <h2 className="sr-only">상태 관리</h2>
+      <AccordionItem title="넷플릭스는 무엇인가요?">
+        <p>넷플릭스는</p>
+        <p>넷플릭스입니다.</p>
+      </AccordionItem>
+      <AccordionItem title="넷플릭스 요금은 얼마인가요?">
+        <p>넷플릭스 요금은</p>
+        <p>검색해보세요.</p>
+      </AccordionItem>
+    </section>
+  );
 }
 
 export default StateManagement;
+
+function AccordionItem({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) {
+  const [isVisible, setIsvisible] = useState(false);
+
+  const handleToggle = () => setIsvisible((v) => !v);
+
+  return (
+    <div className="flex flex-col space-y-0.5">
+      <button type="button" onClick={handleToggle}>
+        {title}
+      </button>
+      <div className={tm({ hidden: !isVisible })}>{children}</div>
+    </div>
+  );
+}
