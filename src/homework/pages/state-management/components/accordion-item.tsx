21baebb8b82b6f46271159699338068e8931504e
kongsh <ksh080838@gmail.com>
2025-02-04 09:50:17 +0900

    [실습] CSS 트랜지션 + @starting-style 활용
    - 아코디언 트랜지션 구현
    - tailwind CSS 유틸리티 클래스 활용

--- src/homework/pages/state-management/components/accordion-item.tsx
+++ src/homework/pages/state-management/components/accordion-item.tsx
@@ -1,23 +1,46 @@
 import { tm } from '@/utils/tw-merge';
 import { useState } from 'react';
 
-function AccordionItem({
-  title,
-  children,
-}: {
+interface AccordionItemProps {
   title: string;
   children: React.ReactNode;
-}) {
+}
+
+function AccordionItem({ title, children }: AccordionItemProps) {
   const [isVisible, setIsvisible] = useState(false);
 
   const handleToggle = () => setIsvisible((v) => !v);
 
   return (
-    <div className="flex flex-col space-y-0.5">
-      <button type="button" onClick={handleToggle}>
+    <div className={tm('flex flex-col space-y-2', 'mb-8')}>
+      <button
+        type="button"
+        className={tm(
+          'text-xl font-medium text-primary-500',
+          'cursor-pointer',
+          'hover:text-primary-700'
+        )}
+        onClick={handleToggle}
+      >
         {title}
       </button>
-      <div className={tm({ hidden: !isVisible })}>{children}</div>
+      <div
+        className={tm(
+          { hidden: !isVisible },
+          'text-sm text-slate-800 leading-[1.5]',
+          '*:mb-3',
+          // [from]
+          'opacity-50 -translate-y-2',
+          // start
+          'starting:opacity-0 starting:-translate-y-2',
+          // 전환
+          'transition-all transition-discrete duration-500',
+          // [to]
+          { 'opacity-100 translate-y-0': isVisible }
+        )}
+      >
+        {children}
+      </div>
     </div>
   );
 }
