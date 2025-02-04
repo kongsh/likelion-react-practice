a7ee2effe1412f2e03d84f3da40868288d0b4721
kongsh <ksh080838@gmail.com>
2025-02-04 09:21:18 +0900

    [실습] getViewElement 함수 작성
    - 컴포넌트 내부에서 1회 실행되는 로직을 별도 함수로 분리

--- src/playground.tsx
+++ src/playground.tsx
@@ -6,32 +6,36 @@ import HomeworkSignUpForm from '@/homework/pages/sign-up';
 import StateManagement from '@/homework/pages/state-management';
 import { getUIView, type UIView } from '@/homework/lib/ui-view';
 
-function Playground() {
-  const [uiView] = useState<UIView>(getUIView);
-
-  let ViewComponent: React.ReactElement | null = null;
-
+const getViewComponent = (uiView: UIView) => {
+  let viewElement: React.ReactElement | null = null;
   switch (uiView) {
     case 'signin': {
-      ViewComponent = <HomeworkSignInForm />;
+      viewElement = <HomeworkSignInForm />;
       break;
     }
     case 'signup': {
-      ViewComponent = <HomeworkSignUpForm />;
+      viewElement = <HomeworkSignUpForm />;
       break;
     }
     case 'state-management': {
-      ViewComponent = <StateManagement />;
+      viewElement = <StateManagement />;
       break;
     }
   }
+  return viewElement;
+};
+
+function Playground() {
+  const [uiView] = useState<UIView>(getUIView);
+
+  const viewElement = getViewComponent(uiView);
 
   return (
     <div className="Playground bg-euid-gray-200 wrapper">
       <h1>플레이그라운드</h1>
       <Nav />
 
-      {ViewComponent}
+      {viewElement}
     </div>
   );
 }
