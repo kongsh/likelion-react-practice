1fe6ae8aafdde9f79265dc9a97ada2ba77a4715c
kongsh <ksh080838@gmail.com>
2025-02-07 14:17:13 +0900

    [실습] 오류 경계 진행
    - componentDidCatch
    - errorInfo정보를 화면에 렌더링

--- src/components/error-boundary/index.tsx
+++ src/components/error-boundary/index.tsx
@@ -1,3 +1,4 @@
+import { tm } from '@/utils/tw-merge';
 import { Component, ErrorInfo } from 'react';
 
 interface Props {
@@ -17,21 +18,44 @@ class ErrorBoundary extends Component<Props, State> {
     hasError: false,
   };
 
-  static getDerivedStateFromError(error: Error) {
-    return {
+  // static getDerivedStateFromError(error: Error) {
+  //   return {
+  //     hasError: true,
+  //     error,
+  //   };
+  // }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    this.setState({
       hasError: true,
       error,
-    };
+      errorInfo,
+    });
   }
 
   render() {
     if (this.state.hasError) {
-      const { error } = this.state;
+      const { error, errorInfo } = this.state;
 
       return (
-        <div role="alert" lang="en">
-          <h2>{error?.name} 오류 발생</h2>
-          <p>{error?.message}</p>
+        <div
+          role="alert"
+          lang="en"
+          className={tm(
+            'overflow-x-scroll',
+            'flex flex-col gap-2',
+            'border-4 border-red-600',
+            ' text-red-600',
+            'p-5'
+          )}
+        >
+          <h2 className={tm('text-xl font-semibold')}>
+            {error?.name} 오류 발생
+          </h2>
+          <p className={tm('text-red-700 font-normal')}>{error?.message}</p>
+          <pre className="-ml-8 text-xs leading-[2]">
+            {errorInfo?.componentStack}
+          </pre>
         </div>
       );
     }
