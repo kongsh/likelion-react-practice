3743f7d7469ba480905a83ca4ad819283616a9d6
kongsh <ksh080838@gmail.com>
2025-02-11 11:16:13 +0900

    [실습] 리스트 렌더링 시 key 속성 값에 index를 사용하면 안되는 이유
    - lodash의 shuffle 함수 활용
    - 리스트 셔플

--- src/pages/side-effect/index.tsx
+++ src/pages/side-effect/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useId, useState } from 'react';
-import throttle from '@/utils/throttle';
+import throttle from 'lodash-es/throttle';
 import { tm } from '@/utils/tw-merge';
-import debounce from '@/utils/debounce';
+import debounce from 'lodash-es/debounce';
+import shuffle from 'lodash-es/shuffle';
 
 function SideEffectDemo() {
   const throttleTimeId = useId();
@@ -29,6 +30,20 @@ function SideEffectDemo() {
       globalThis.removeEventListener('pointermove', handleMove);
     };
   }, [throttleTime]);
+
+  // const [list, setList] = useState([1, 2, 3, 4, 5]);
+  const [list, setList] = useState([
+    { id: `subject - 1`, defaultValue: 'react' },
+    { id: `subject - 2`, defaultValue: 'react-dom' },
+    { id: `subject - 3`, defaultValue: 'react-router' },
+    { id: `subject - 4`, defaultValue: 'zustand' },
+  ]);
+
+  console.log({ list });
+
+  const handleShuffleList = () => {
+    setList(shuffle(list));
+  };
 
   return (
     <section className="flex flex-col items-start">
@@ -62,6 +77,39 @@ function SideEffectDemo() {
         <span className="font-thin mx-3">/</span> y{' '}
         <span className="font-thin mx-3">=</span> {mouse.y}
       </output>
+
+      <h2 className="mt-6 text-2xl font-medium">
+        key 속성에 index를 사용하면 안되는 이유
+      </h2>
+
+      <button
+        type="button"
+        onClick={handleShuffleList}
+        className={tm(
+          'cursor-pointer',
+          'inline-flex justify-center',
+          'my-5 py-3 px-7 rounded-full',
+          'bg-black text-white text-base font-extrabold',
+          'active:scale-96 active:opacity-80'
+        )}
+      >
+        리스트 셔플
+      </button>
+
+      <ul className="*:text-lg *:font-semibold">
+        {list.map((item, index) => (
+          <li key={item.id}>
+            <span className="block mb-2">
+              {index} / [{item.id}]
+            </span>
+            <input
+              type="text"
+              defaultValue={item.defaultValue}
+              className="p-2 bg-white rounded-md"
+            />
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
