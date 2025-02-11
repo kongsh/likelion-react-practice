7a793d4dec3dc6878f22cdb043c31dfd8f0640a2
kongsh <ksh080838@gmail.com>
2025-02-11 10:42:07 +0900

    [실습] 이벤트 발생 빈도 조절(JSX)
    - value 대신 defaultValue 사용 필요

--- src/pages/side-effect/index.tsx
+++ src/pages/side-effect/index.tsx
@@ -1,51 +1,66 @@
+import { useEffect, useId, useState } from 'react';
 import throttle from '@/utils/throttle';
-import { useEffect, useState } from 'react';
+import { tm } from '@/utils/tw-merge';
+import debounce from '@/utils/debounce';
 
 function SideEffectDemo() {
-  const [count, setCount] = useState(10);
-
-  const doubleCount = count ** 2;
-
-  const handleChangeCount = () => setCount((c) => c + 2);
-
-  useEffect(() => {
-    console.log({ updatedCountValue: count, doubleCount });
-  }, [count, doubleCount]);
-
-  // ------------------------
-
+  const throttleTimeId = useId();
   const [mouse, setMouse] = useState({ x: 0, y: 0 });
+  const [throttleTime, setThrottleTime] = useState(200);
 
-  const mouseOver = mouse.x > window.innerWidth / 2;
-
-  useEffect(() => {
-    console.log(mouseOver);
-  }, [mouseOver]);
+  // JSX에 연결되는 이벤트 핸들러의 경우 throttle/debounce를 사용
+  // 이벤트 발생 빈도 조절 -> defaultValue 속성 사용
+  const handleChangeThrottleTime = debounce(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const nextThrottleTime = Number(e.target.value);
+      setThrottleTime(nextThrottleTime);
+    },
+    300
+  );
 
   useEffect(() => {
     const handleMove = throttle((e: PointerEvent) => {
       setMouse({ x: +e.clientX.toFixed(0), y: +e.clientY.toFixed(0) });
-    }, 200);
+    }, throttleTime);
 
     globalThis.addEventListener('pointermove', handleMove);
 
     return () => {
       globalThis.removeEventListener('pointermove', handleMove);
     };
-  }, []);
+  }, [throttleTime]);
 
   return (
-    <section className="*:text-slate-800 flex flex-col space-y-10">
-      <h2 className="text-2xl font-medium mb-2">React.useEffect 훅 함수</h2>
-      <button
-        type="button"
-        className="px-2 py-1 border text-xl"
-        onClick={handleChangeCount}
+    <section className="flex flex-col items-start">
+      <h2 className="text-2xl font-medium">마우스 포인터 움직임 조절</h2>
+
+      <div className="mt-5 mb-1">
+        <label htmlFor={throttleTimeId}>이벤트 발생 빈도 조절</label>
+        <div className={tm('flex gap-1')}>
+          <input
+            type="range"
+            id={throttleTimeId}
+            min={10}
+            max={1000}
+            defaultValue={throttleTime}
+            step={10}
+            className="accent-black"
+            onChange={handleChangeThrottleTime}
+          />
+          <output>{throttleTime / 1000}s</output>
+        </div>
+      </div>
+
+      <output
+        className={tm(
+          'inline-flex justify-center',
+          'my-5 py-3 px-7 rounded-full',
+          'bg-black text-white text-2xl'
+        )}
       >
-        {count}
-      </button>
-      <output className="inline-flex my-5 py-3 px-5 border-2 text-2xl">
-        x = {mouse.x} / y = {mouse.y}
+        x <span className="font-thin mx-3">=</span> {mouse.x}{' '}
+        <span className="font-thin mx-3">/</span> y{' '}
+        <span className="font-thin mx-3">=</span> {mouse.y}
       </output>
     </section>
   );
