0d568ce8c85afd17d7ec407bf8854dc9b59a7061
kongsh <ksh080838@gmail.com>
2025-02-11 15:41:57 +0900

    [실습] 검색 리스트
    - 실시간 검색 기능 구현

--- src/pages/search-list/components/searched-list.tsx
+++ src/pages/search-list/components/searched-list.tsx
@@ -3,16 +3,26 @@ import { ColorMoodItem } from '../types';
 import Card from './card';
 
 interface SearchedListProps {
+  query: string;
   list: ColorMoodItem[];
   onUpdate: (item: ColorMoodItem, isFavorited: boolean) => void;
 }
 
-function SearchedList({ list, onUpdate }: SearchedListProps) {
+function SearchedList({ query, list, onUpdate }: SearchedListProps) {
+  const word = query.toLowerCase();
+
+  const filteredList = list.filter(
+    (item) =>
+      item.title.includes(word) ||
+      item.description.includes(word) ||
+      item.tags.includes(word)
+  );
+
   return (
     <section>
       <h3 className="sr-only">검색된 리스트</h3>
       <ul className={tm('flex flex-col gap-12')}>
-        {list.map((item) => (
+        {filteredList.map((item) => (
           <Card key={item.id} item={item} onUpdate={onUpdate} />
         ))}
       </ul>
--- src/pages/search-list/index.tsx
+++ src/pages/search-list/index.tsx
@@ -3,28 +3,30 @@ import SearchedList from './components/searched-list';
 import { useState } from 'react';
 import colorMoodList from './data/color-mood-list';
 import { ColorMoodItem, ColorMoodList } from './types';
+import { tm } from '@/utils/tw-merge';
 
 function SearchListPage() {
   const [list, setList] = useState<ColorMoodList>(colorMoodList);
+  const [query, setQuery] = useState('');
 
   const handleUpdateList = (item: ColorMoodItem, isFavorited: boolean) => {
-    const nextList = list.map((it) => {
-      return it.id === item.id
+    const nextList = list.map((it) =>
+      it.id === item.id
         ? {
             ...it,
             isFavorited,
           }
-        : it;
-    });
+        : it
+    );
 
     setList(nextList);
   };
 
   return (
-    <section>
-      <h2 className="text-2xl font-semibold text-react">카드 검색 리스트</h2>
-      <SearchForm />
-      <SearchedList list={list} onUpdate={handleUpdateList} />
+    <section className={tm('flex flex-col gap-5 items-center')}>
+      <h2 className="text-2xl font-medium text-react">카드 검색 리스트</h2>
+      <SearchForm query={query} setQuery={setQuery} />
+      <SearchedList query={query} list={list} onUpdate={handleUpdateList} />
     </section>
   );
 }
