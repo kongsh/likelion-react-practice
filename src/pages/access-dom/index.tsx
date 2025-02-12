b9bebfcc9561b1a7a27c476df4e5895185848e24
kongsh <ksh080838@gmail.com>
2025-02-12 15:44:15 +0900

    [실습] DOM 접근/조작
    - JSX + ref 콜백 함수
    - DOM 마운트 이후 함수 호출

--- src/pages/access-dom/index.tsx
+++ src/pages/access-dom/index.tsx
@@ -1,4 +1,4 @@
-import { createElement, useState } from 'react';
+import { useState } from 'react';
 
 function AccessDOMPage() {
   // 컴포넌트 바디(body)
@@ -16,46 +16,35 @@ function AccessDOMPage() {
   // - ref 콜백 함수
 
   // JSX -> React API (JavaScript) 코드 컴파일 -> 브라우저 해석(parsing) -> UI 렌더링 (브라우저 페인팅) => 사용자
-  return createElement(
-    'section',
-    {
-      ref: (element) => {
-        console.log('section', { element });
-      },
-    },
-    createElement(
-      'h2',
-      {
-        ref: (element) => {
-          console.log('section > h2', { element });
-        },
-        className: 'text-2xl text-react font-medium',
-      },
-      createElement(
-        'abbr',
-        {
-          ref: (element) => {
-            console.log('section > h2 > abbr', { element });
-          },
-          title: 'Document Object Model',
-          className: 'cursor-help no-underline',
-        },
-        isParse ? 'Document Object Model' : 'DOM'
-      ),
-      ' ',
-      '접근/조작'
-    ),
-    createElement(
-      'button',
-      {
-        type: 'button',
-        className: 'cursor-pointer px-2 py-0.5 bg-black text-white',
-        onClick: () => {
+  return (
+    <section
+      ref={(element) => {
+        if (element) {
+          element.style.cssText = `
+          padding: 40px;
+          border: 4px solid black;
+        `;
+        }
+      }}
+    >
+      <h2 className="text-2xl text-react font-medium">
+        <abbr
+          title="Document Object Model"
+          className="cursor-help no-underline"
+        >
+          {isParse ? 'Document Object Model' : 'DOM'}
+        </abbr>
+        접근 조작
+      </h2>
+      <button
+        type="button"
+        onClick={() => {
           setIsParse((p) => !p);
-        },
-      },
-      'toggle button'
-    )
+        }}
+      >
+        DOM 용어 풀이
+      </button>
+    </section>
   );
 }
 
