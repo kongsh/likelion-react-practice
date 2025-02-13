d8716885d26008f45c1dfa317b15e73dcad6873e
kongsh <ksh080838@gmail.com>
2025-02-13 13:49:11 +0900

    [실습] 다른 컴포넌트 DOM 조작
    - react 18 이하 : forwardRef 사용
    - 19 부터 : ref를 props처럼 넘겨줌

--- src/pages/access-dom/index.tsx
+++ src/pages/access-dom/index.tsx
@@ -1,27 +1,24 @@
 import { useEffect, useRef, useState } from 'react';
-import { Search } from '@mynaui/icons-react';
-import { tm } from '@/utils/tw-merge';
 import TiltBox from './components/tilt-box';
+import SearchInput from './components/search-input';
 
 function AccessDOMPage() {
-  const abbrRef = useRef<HTMLElement>(null);
-  console.log(0, { 'abbrRef.current': abbrRef.current });
-
-  useEffect(() => {
-    if (abbrRef.current) {
-      console.log(1, { 'abbrRef.current': abbrRef.current });
-    }
-  }, []);
-
   const [isParse, setIsParse] = useState(false);
 
   const searchInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (searchInputRef.current) {
+        searchInputRef.current.focus();
+      }
+    }, 1000);
+  }, []);
 
   return (
     <section>
       <h2 className="text-2xl text-react font-medium">
         <abbr
-          ref={abbrRef}
           title="Document Object Model"
           className="cursor-help no-underline"
         >
@@ -39,29 +36,7 @@ function AccessDOMPage() {
       </button>
 
       <form className="my-10 flex">
-        <div>
-          <label htmlFor="like-a-book" className="sr-only">
-            선호 도서
-          </label>
-          <input
-            ref={searchInputRef}
-            id="like-a-book"
-            type="search"
-            placeholder="좋아하는 도서는?"
-            className="bg-react text-white px-3 py-2"
-          />
-        </div>
-        <button
-          type="submit"
-          aria-label="검색"
-          className={tm(
-            'cursor-pointer',
-            'py-2 w-20 grid place-items-center',
-            'bg-cyan-700 text-white '
-          )}
-        >
-          <Search />
-        </button>
+        <SearchInput ref={searchInputRef} />
       </form>
 
       <div className="flex flex-wrap">
