148ebec4c56aff47a8a302c6abfdc17a430f25fb
kongsh <ksh080838@gmail.com>
2025-02-21 11:21:38 +0900

    [실습] 상태 구독, 변경, 파생된 상태 업데이트
    - subscribeWithSelector 미들웨어
    - 구독 및 구독 해지
    - 셀렉터(selector) 활용
    - 리스너

--- src/pages/app-state-management/components/count-display.tsx
+++ src/pages/app-state-management/components/count-display.tsx
@@ -2,9 +2,20 @@ import { useCountStore } from '@/stores/count';
 import { memo } from 'react';
 
 function CountDisplay() {
-  const count = useCountStore(({ count }) => count);
+  const { count, square } = useCountStore();
 
-  return <output className="font-black text-3xl">{count}</output>;
+  return (
+    <div className="flex gap-2 flex-col items-center">
+      <div className="flex gap-1 items-center">
+        <span>카운트(count)</span>
+        <output className="font-black text-3xl">{count}</output>
+      </div>
+      <div className="flex gap-1 items-center">
+        <span>제곱(square)</span>
+        <output className="font-black text-3xl">{square}</output>
+      </div>
+    </div>
+  );
 }
 
 export default memo(CountDisplay);
--- src/stores/count.ts
+++ src/stores/count.ts
@@ -1,5 +1,10 @@
 import { create } from 'zustand';
-import { combine, devtools, persist } from 'zustand/middleware';
+import {
+  combine,
+  devtools,
+  persist,
+  subscribeWithSelector,
+} from 'zustand/middleware';
 
 const initialState = {
   count: 1,
@@ -8,40 +13,55 @@ const initialState = {
   max: 100,
 };
 
+const derivedState = {
+  square: initialState.count ** 2,
+};
+
 export const useCountStore = create(
-  // 개발 도구 미들웨어
-  persist(
-    devtools(
-      combine({ ...initialState }, (set) => {
-        return {
-          increment: () =>
-            set(
-              ({ count, step, max }) => ({
-                count: count + step > max ? max : count + step,
-              }),
-              undefined,
-              'increment'
-            ),
-          decrement: () =>
-            set(
-              ({ count, step, min }) => ({
-                count: count - step < min ? min : count - step,
-              }),
-              undefined,
-              'decrement'
-            ),
-          reset: () => set(initialState, undefined, 'reset'),
-          update: (value: number) =>
-            set(
-              ({ max, min }) => ({
-                count: value > max ? max : value < min ? min : value,
-              }),
-              undefined,
-              'update'
-            ),
-        };
-      })
-    ),
-    { name: 'store/counter' }
+  subscribeWithSelector(
+    persist(
+      devtools(
+        combine({ ...initialState, ...derivedState }, (set) => {
+          return {
+            increment: () =>
+              set(
+                ({ count, step, max }) => ({
+                  count: count + step > max ? max : count + step,
+                }),
+                undefined,
+                'increment'
+              ),
+            decrement: () =>
+              set(
+                ({ count, step, min }) => ({
+                  count: count - step < min ? min : count - step,
+                }),
+                undefined,
+                'decrement'
+              ),
+            reset: () => set(initialState, undefined, 'reset'),
+            update: (value: number) =>
+              set(
+                ({ max, min }) => ({
+                  count: value > max ? max : value < min ? min : value,
+                }),
+                undefined,
+                'update'
+              ),
+          };
+        })
+      ),
+      { name: 'store/counter' }
+    )
   )
+);
+
+// 상태 변경 구독
+useCountStore.subscribe(
+  // 셀렉터
+  (s) => s.count,
+  // 리스너
+  (count) => {
+    useCountStore.setState({ square: count ** 2 });
+  }
 );
