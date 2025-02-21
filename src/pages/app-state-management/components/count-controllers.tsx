216cda379931ced6a97d5d29e467145a3eb44f41
kongsh <ksh080838@gmail.com>
2025-02-21 11:04:54 +0900

    [실습] 리렌더가 일어나는 컴포넌트
    - 구조분해할당으로 값을 가져와도 무방함

--- src/pages/app-state-management/components/count-controllers.tsx
+++ src/pages/app-state-management/components/count-controllers.tsx
@@ -7,17 +7,9 @@ function CountControllers() {
   const increment = useCountStore(({ increment }) => increment);
   const decrement = useCountStore(({ decrement }) => decrement);
   const reset = useCountStore(({ reset }) => reset);
-  const update = useCountStore(({ update }) => update);
 
   return (
     <div className="flex gap-1">
-      <input
-        type="number"
-        aria-label="카운트 값"
-        className="border"
-        defaultValue={1}
-        onChange={(e) => update(Number(e.currentTarget.value))}
-      />
       <button
         type="button"
         className={tm(
--- src/pages/app-state-management/components/counter.tsx
+++ src/pages/app-state-management/components/counter.tsx
@@ -1,12 +1,23 @@
 import { memo } from 'react';
 import CountDisplay from './count-display';
 import CountControllers from './count-controllers';
+import { useCountStore } from '@/stores/count';
 
 function Counter() {
+  const { min, max, count, update } = useCountStore();
+
   return (
     <div className="flex flex-col gap-2 items-center">
       <CountDisplay />
-
+      <input
+        type="number"
+        aria-label="카운트 값"
+        className="border"
+        min={min}
+        max={max}
+        value={count}
+        onChange={(e) => update(Number(e.currentTarget.value))}
+      />
       <CountControllers />
     </div>
   );
